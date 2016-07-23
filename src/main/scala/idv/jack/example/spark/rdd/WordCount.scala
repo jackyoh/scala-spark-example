ee014e177cac64d9baa121505cae90ee40783d9a
jack <yangj26952@gmail.com>
2016-07-23 23:57:20 +0800

    add spark code for scala

---
+++ src/main/scala/idv/jack/example/spark/rdd/WordCount.scala
@@ -0,0 +1,27 @@
+package idv.jack.example.spark.rdd
+import org.apache.spark._
+
+object WordCount {
+  
+  def main(args: Array[String]){
+       val sparkConf = new SparkConf().setAppName("Word Count Test")
+                                      .setMaster("local")
+                                      
+       val sc = new SparkContext(sparkConf)
+       val sourceRDD = sc.textFile("/home/user1/wordcount.txt")
+       
+       val flatMapRDD = sourceRDD.flatMap(f => f.split(" "))
+       val mapToPairRDD = flatMapRDD.map(word => (word, 1))
+       val reduceRDD = mapToPairRDD.reduceByKey((x, y) => x + y)
+       
+       val resultList = reduceRDD.collect()
+       
+       for(result <- resultList){
+          println(result._1 + ":" + result._2)
+         }
+       
+       
+  }
+  
+  
+}
